172deb41858a1bd503e2ba220bb7b07b536bfd39
Miguel Angel Alonso Perez <Miguel A. Alonso>
2017-01-17 00:08:55 +0100

    feat(sims): add sims (users) load

--- index.js
+++ index.js
@@ -1,40 +1,99 @@
 'use strict';
 
+const ROOT = `${__dirname}`;
+global.requireFromRoot = function (path) {
+  return require(`${ROOT}/${path}`);
+};
+
 var app = require('connect')();
 var http = require('http');
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
 var fs = require('fs');
-var serverPort = 8080;
+var log = require("winston");
+var path = require("path");
 
-// swaggerRouter configuration
-var options = {
-  swaggerUi: '/swagger.json',
-  controllers: './controllers',
-  useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
-};
+var config = requireFromRoot("lib/config")
+var sims = requireFromRoot("lib/sims")
 
-// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
-var spec = fs.readFileSync('./api/swagger/swagger.yaml', 'utf8');
-var swaggerDoc = jsyaml.safeLoad(spec);
+let main = function main(argv) {
 
-// Initialize the Swagger middleware
-swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
-  // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
-  app.use(middleware.swaggerMetadata());
+  // swaggerRouter configuration
+  var options = {
+    swaggerUi: '/swagger.json',
+    controllers: './controllers',
+    useStubs: false // Conditionally turn on stubs (mock mode)
+  };
 
-  // Validate Swagger requests
-  app.use(middleware.swaggerValidator());
+  // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+  var spec = fs.readFileSync('./api/swagger/swagger.yaml', 'utf8');
+  var swaggerDoc = jsyaml.safeLoad(spec);
 
-  // Route validated requests to appropriate controller
-  app.use(middleware.swaggerRouter(options));
+  //Load users data
+  sims.load(config.dataPath);
 
-  // Serve the Swagger documents and Swagger UI
-  app.use(middleware.swaggerUi());
+  // Initialize the Swagger middleware
+  swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+    // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+    app.use(middleware.swaggerMetadata());
 
-  // Start the server
-  http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+    // Validate Swagger requests
+    app.use(middleware.swaggerValidator());
+
+    // Route validated requests to appropriate controller
+    app.use(middleware.swaggerRouter(options));
+
+    // Serve the Swagger documents and Swagger UI
+    app.use(middleware.swaggerUi());
+
+    // Start the server
+    http.createServer(app).listen(config.port, function () {
+      console.log('Your server is listening on port %d (http://localhost:%d)', config.port, config.port);
+      console.log('Swagger-ui is available on http://localhost:%d/docs', config.port);
+    });
   });
-});
+}
+
+if (require.main === module) {
+
+  let argv = require('minimist')(process.argv.slice(2));
+
+  //help
+  if(argv.h || argv.help) {
+    console.log("Usage:");
+    console.log("   npm start -- --config=<path-to-config-file>");
+    return 0;
+  }
+
+  if(!argv.config) {
+    console.log("Error! Missing config file.")
+    console.log("Usage:");
+    console.log("   npm start -- [-h|--help] --config=<path_to_config_file>");
+    return 1
+  }
+
+  let configFileName = path.normalize(__dirname + '/' + argv.config);
+  log.info("Loading config file: " + configFileName);
+  //parsing config file
+  try {
+    if(fs.lstatSync(configFileName).isFile()) {
+      config.load(require(__dirname + '/' + argv.config));
+    } else {
+      throw `Error! ${configFileName} it's not a file`;
+    }
+  }
+  catch(e) {
+    log.error("Can't open file: " + configFileName)
+    throw e
+  }
+
+  //logger level
+  if(config.logLevel) {
+    log.info("Setting log level to: " + config.logLevel);
+    log.level = config.logLevel;
+  } else {
+    log.level='silent';
+  }
+
+  main(process.argv.slice(2, process.argv.length));
+}
